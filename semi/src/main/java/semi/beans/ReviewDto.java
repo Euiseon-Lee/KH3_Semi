f0d704b097fe7dc8df2ebca475525d092f855272
Euiseon <inia8inia@gmail.com>
2022-05-03 16:44:26 +0900

    리뷰게시판 dto 생성완료

--- semi/src/main/java/semi/beans/ReviewDto.java
+++ semi/src/main/java/semi/beans/ReviewDto.java
@@ -1,5 +1,87 @@
 package semi.beans;
 
-public class ReviewDto {
+import java.sql.Date;
 
+
+public class ReviewDto {
+	private long reviewNo;
+	private String reviewMemberId;
+	private int reviewRoomNo;
+	private String reviewTitle;
+	private String reviewContent;
+	private Date reviewWritedate;
+	private long reviewReadcount;
+	private int reviewRate;
+	private long reviewLikes;
+	
+	public ReviewDto() {
+		super();
+	}
+	
+	
+	public long getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(long reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public String getReviewMemberId() {
+		return reviewMemberId;
+	}
+	public void setReviewMemberId(String reviewMemberId) {
+		this.reviewMemberId = reviewMemberId;
+	}
+	public int getReviewRoomNo() {
+		return reviewRoomNo;
+	}
+	public void setReviewRoomNo(int reviewRoomNo) {
+		this.reviewRoomNo = reviewRoomNo;
+	}
+	public String getReviewTitle() {
+		return reviewTitle;
+	}
+	public void setReviewTitle(String reviewTitle) {
+		this.reviewTitle = reviewTitle;
+	}
+	public String getReviewContent() {
+		return reviewContent;
+	}
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+	public Date getReviewWritedate() {
+		return reviewWritedate;
+	}
+	public void setReviewWritedate(Date reviewWritedate) {
+		this.reviewWritedate = reviewWritedate;
+	}
+	public long getReviewReadcount() {
+		return reviewReadcount;
+	}
+	public void setReviewReadcount(long reviewReadcount) {
+		this.reviewReadcount = reviewReadcount;
+	}
+	public int getReviewRate() {
+		return reviewRate;
+	}
+	public void setReviewRate(int reviewRate) {
+		this.reviewRate = reviewRate;
+	}
+	public long getReviewLikes() {
+		return reviewLikes;
+	}
+	public void setReviewLikes(long reviewLikes) {
+		this.reviewLikes = reviewLikes;
+	}
+	
+	@Override
+	public String toString() {
+		return "ReviewDto [reviewNo=" + reviewNo + ", reviewMemberId=" + reviewMemberId + ", reviewRoomNo="
+				+ reviewRoomNo + ", reviewTitle=" + reviewTitle + ", reviewContent=" + reviewContent
+				+ ", reviewWritedate=" + reviewWritedate + ", reviewReadcount=" + reviewReadcount + ", reviewRate="
+				+ reviewRate + ", reviewLikes=" + reviewLikes + "]";
+	}
+
+
+	
 }
