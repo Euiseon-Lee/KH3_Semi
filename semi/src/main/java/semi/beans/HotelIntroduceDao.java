365940342e98794e1d4144f7384d2fcf10a95cbe
gkstjr <gkstjr8332@naver.com>
2022-05-04 00:37:03 +0900

    호텔소개 dao dto 수정

--- semi/src/main/java/semi/beans/HotelIntroduceDao.java
+++ semi/src/main/java/semi/beans/HotelIntroduceDao.java
@@ -1,5 +1,35 @@
 package semi.beans;
 
-public class HotelIntroduceDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+public class HotelIntroduceDao {
+	
+	//목록
+	public List<HotelIntroduceDto> selectList() throws Exception{
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from hotel_introduce";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		List<HotelIntroduceDto> list = new ArrayList<>();
+		
+		while(rs.next()) {
+			HotelIntroduceDto hotelIntroduceDto = new HotelIntroduceDto();
+			
+			hotelIntroduceDto.setHotelIntroduceWriter(rs.getString("hotel_introduce_writer"));
+			hotelIntroduceDto.setHotelIntroduceHead(rs.getString("hotel_introduce_head"));
+			hotelIntroduceDto.setHotelIntroduceContent(rs.getString("hotel_introduce_content"));
+			
+			list.add(hotelIntroduceDto);
+		}
+	
+			con.close();
+			
+			return list;
+	}
 }
--- semi/src/main/java/semi/beans/HotelIntroduceDto.java
+++ semi/src/main/java/semi/beans/HotelIntroduceDto.java
@@ -1,5 +1,30 @@
 package semi.beans;
 
 public class HotelIntroduceDto {
-
+	private String hotelIntroduceWriter;
+	private String hotelIntroduceHead;
+	private String hotelIntroduceContent;
+	public String getHotelIntroduceWriter() {
+		return hotelIntroduceWriter;
+	}
+	public void setHotelIntroduceWriter(String hotelIntroduceWriter) {
+		this.hotelIntroduceWriter = hotelIntroduceWriter;
+	}
+	public String getHotelIntroduceHead() {
+		return hotelIntroduceHead;
+	}
+	public void setHotelIntroduceHead(String hotelIntroduceHead) {
+		this.hotelIntroduceHead = hotelIntroduceHead;
+	}
+	public String getHotelIntroduceContent() {
+		return hotelIntroduceContent;
+	}
+	public void setHotelIntroduceContent(String hotelIntroduceContent) {
+		this.hotelIntroduceContent = hotelIntroduceContent;
+	}
+	public HotelIntroduceDto() {
+		super();
+	}
+	
+	
 }
