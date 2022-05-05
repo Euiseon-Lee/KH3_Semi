c4ffafa074c9e2be69a95c88f0e569cf35a60f52
gkstjr <gkstjr8332@naver.com>
2022-05-05 20:14:35 +0900

    리뷰게시판 댓글 insert DAO

--- semi/src/main/java/semi/beans/ReviewReplyDao.java
+++ semi/src/main/java/semi/beans/ReviewReplyDao.java
@@ -1,8 +1,26 @@
 package semi.beans;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class ReviewReplyDao {
 	
-	
-	//테스트 커밋
+	//리뷰 게시판 댓글 생성
+	public void insert(ReviewReplyDto reviewReplyDto) throws Exception{
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "insert into review_reply(reply_no, reply_writer, reply_target,reply_content) "
+					+ "values(reply_seq.nextval,?,?,?)";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, reviewReplyDto.getReplyWriter());
+		ps.setInt(2, reviewReplyDto.getReplyTarget());
+		ps.setString(3, reviewReplyDto.getReplyContent());
+		ps.execute();
+		
+		con.close();
+		
+	}
+
 
 }
