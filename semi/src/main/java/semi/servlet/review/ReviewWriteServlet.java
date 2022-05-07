d73a1d5830e113e50c823c98f2af3dd25ca99412
Euiseon <inia8inia@gmail.com>
2022-05-07 17:09:25 +0900

    리뷰 글작성 servlet
    - 테스트용 코드 추가
    - 인코딩 코드 추가

--- semi/src/main/java/semi/servlet/review/ReviewWriteServlet.java
+++ semi/src/main/java/semi/servlet/review/ReviewWriteServlet.java
@@ -20,26 +20,39 @@ public class ReviewWriteServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
 			
+			//필터 만들면 지우기
+			req.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html; charset=utf-8");
+			
+			
+			
 			ReviewDto reviewDto = new ReviewDto();			
 			
-			//추후 세션을 만들면 reviewMemberId 가져올 코드
-			String memberId = (String) req.getSession().getAttribute("login");
-			reviewDto.setReviewMemberId(memberId);
-			
-
 			reviewDto.setReviewTitle(req.getParameter("reviewTitle"));
 			reviewDto.setReviewContent(req.getParameter("reviewContent"));
 			reviewDto.setReviewStar(Integer.parseInt(req.getParameter("reviewStar")));
 			
+			//추후 세션을 만들면 reviewMemberId 가져올 코드
+			//String memberId = (String) req.getSession().getAttribute("login");
+			//reviewDto.setReviewMemberId(memberId);
 			
-			//수정필요
-			reviewDto.setReviewOrderNo(Integer.parseInt(req.getParameter("reviewOrderNo")));	
-
+			
+			//주문번호 불러오는 코드
+			//reviewDto.setReviewOrderNo(Integer.parseInt(req.getParameter("reviewOrderNo")));	
+			
+			//세션과 PayDto 불러올수 있을 때 삭제할 코드
+			reviewDto.setReviewOrderNo(2);
+			reviewDto.setReviewMemberId("testmanager");
 			
 			
 			
 			ReviewDao reviewDao = new ReviewDao();
 			reviewDto.setReviewNo(reviewDao.getSequence());
+			
+			reviewDao.write(reviewDto);
+			
+			resp.sendRedirect("detail.jsp?reviewNo="+reviewDto.getReviewNo());
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
