bd483a67397a0003691e62c5ea150c70bf9de5ba
Euiseon <inia8inia@gmail.com>
2022-05-07 17:14:21 +0900

    리뷰 글수정 servlet 생성
    - 인코딩 코드 추가
    - 전체 생성

---
+++ semi/src/main/java/semi/servlet/review/ReviewEditServlet.java
@@ -0,0 +1,49 @@
+package semi.servlet.review;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import semi.beans.ReviewDao;
+import semi.beans.ReviewDto;
+
+@WebServlet(urlPatterns="/review/edit.kh")
+public class ReviewEditServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			
+			//필터 만들면 지우기
+			req.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html; charset=utf-8");
+			
+			
+			ReviewDto reviewDto = new ReviewDto();
+			reviewDto.setReviewTitle(req.getParameter("reviewTitle"));
+			reviewDto.setReviewContent(req.getParameter("reviewContent"));
+			reviewDto.setReviewStar(Integer.parseInt(req.getParameter("reviewStar")));
+			reviewDto.setReviewNo(Integer.parseInt(req.getParameter("reviewNo")));
+
+			ReviewDao reviewDao = new ReviewDao();
+			boolean success = reviewDao.edit(reviewDto);
+			
+			
+			if(success) {
+				resp.sendRedirect("detail.jsp?board="+reviewDto.getReviewNo());
+			}
+			else {
+				resp.sendError(404);
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
