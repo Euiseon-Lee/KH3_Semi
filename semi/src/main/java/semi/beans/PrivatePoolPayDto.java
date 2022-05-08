396ebd86c24b6d94db4b1f8c5c48e90dfb4052e2
yujin2555 <yujin2555@naver.com>
2022-05-08 13:48:15 +0900

    확인용

--- semi/src/main/java/semi/beans/PrivatePoolPayDto.java
+++ semi/src/main/java/semi/beans/PrivatePoolPayDto.java
@@ -4,40 +4,42 @@ import java.sql.Date;
 
 public class PrivatePoolPayDto {
 	
-	 private int pool_no; // 개인풀 방 번호 
-	 private int pool_order_no; // 주문번호
-	 private long pay_poolprice; // 개인풀 이용금액
-	 private Date pay_poolusedate; // 개인풀 이용날짜
+	 private int poolNo; // 개인풀 방 번호 
+	 private int poolOrderNo; // 주문번호
+	 private long payPoolprice; // 개인풀 이용금액
+	 private Date payPoolusedate; // 개인풀 이용날짜
 	 
-	public int getRest_tapool_no() {
-		return pool_no;
+	public int getPoolNo() {
+		return poolNo;
 	}
-	public void setRest_tapool_no(int pool_no) {
-		this.pool_no = pool_no;
+	public void setPoolNo(int poolNo) {
+		this.poolNo = poolNo;
 	}
-	public int getPool_order_no() {
-		return pool_order_no;
+	public int getPoolOrderNo() {
+		return poolOrderNo;
 	}
-	public void setPool_order_no(int pool_order_no) {
-		this.pool_order_no = pool_order_no;
+	public void setPoolOrderNo(int poolOrderNo) {
+		this.poolOrderNo = poolOrderNo;
 	}
-	public long getPay_poolprice() {
-		return pay_poolprice;
+	public long getPayPoolprice() {
+		return payPoolprice;
 	}
-	public void setPay_poolprice(long pay_poolprice) {
-		this.pay_poolprice = pay_poolprice;
+	public void setPayPoolprice(long payPoolprice) {
+		this.payPoolprice = payPoolprice;
 	}
-	public Date getPay_poolusedate() {
-		return pay_poolusedate;
+	public Date getPayPoolusedate() {
+		return payPoolusedate;
 	}
-	public void setPay_poolusedate(Date pay_poolusedate) {
-		this.pay_poolusedate = pay_poolusedate;
+	public void setPayPoolusedate(Date payPoolusedate) {
+		this.payPoolusedate = payPoolusedate;
 	}
 	
 	@Override
 	public String toString() {
-		return "PrivatePoolPayDto [pool_no=" + pool_no + ", pool_order_no=" + pool_order_no
-				+ ", pay_poolprice=" + pay_poolprice + ", pay_poolusedate=" + pay_poolusedate + "]";
+		return "PrivatePoolPayDto [poolNo=" + poolNo + ", poolOrderNo=" + poolOrderNo + ", payPoolprice=" + payPoolprice
+				+ ", payPoolusedate=" + payPoolusedate + "]";
 	}
-
+	 
+	 
+	 
 }
