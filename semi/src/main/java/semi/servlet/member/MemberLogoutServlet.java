ef50fa0f275ef1de83094ef7da7dfd59c957ab43
KimDongSun7 <rla9960@naver.com>
2022-05-12 15:21:06 +0900

    게시판용 css 추가

--- semi/src/main/java/semi/servlet/member/MemberLogoutServlet.java
+++ semi/src/main/java/semi/servlet/member/MemberLogoutServlet.java
@@ -16,7 +16,6 @@ public class MemberLogoutServlet extends HttpServlet{
 			req.getSession().removeAttribute("login"); // 로그아웃 시 session 로그인 정보삭제
 			req.getSession().removeAttribute("auth"); //로그아웃 시 권한 정보 삭제(grade)
 			req.getSession().removeAttribute("id");//로그아웃 시 로그인 아이디 삭제
-			req.getSession().removeAttribute("password");
 			resp.sendRedirect(req.getContextPath()); //메인으로 이동
 			
 		} catch (Exception e) {
